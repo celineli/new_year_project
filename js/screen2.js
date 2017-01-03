6ce33fa57b134c23e4e20c918cd29c203d733076
Loic Pennequin <lo.pennequin@gmail.com>
2017-01-03 11:59:26 +0100

    peaufinage

--- js/screen2.js
+++ js/screen2.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
 	$("#text-wrapper").fadeIn(2500);
-	$("#pix").fadeIn(5000);
 });
+
+var anim = ['vitalité.', 'rire.', 'sérénité.', 'rencontres.', 'créativité.', 'succès.'];
+i = 0,
+$div = $('#word');
+setTimeout(function()
+	{
+	setInterval(function ()
+		{
+		$div.text(anim[i++ % anim.length]);
+		}, 2000);
+	}, 800);
